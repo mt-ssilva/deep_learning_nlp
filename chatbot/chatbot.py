3f110a5d0395205c327f7d5ab3092674008963ea
mt-ssilva <matheus.ssilva@upe.br>
2020-12-18 12:40:42 -0300

    criacao de um dicionario para mapear cada linha com seu ID

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -7,6 +7,12 @@ import time
 #--- Pré-processamento dos dados ---
 #Importação da base de dados
 
-linhas = open('movie_lines.txt').read().split('\n')
-conversas = open('movie_conversations.txt').read().split('\n')
+linhas = open('C:\Desenvolvimento\spyder-workspace\deep_learnig_nlp_chatbot\chatbot\movie_lines.txt').read().split('\n')
+conversas = open('C:\Desenvolvimento\spyder-workspace\deep_learnig_nlp_chatbot\chatbot\movie_conversations.txt').read().split('\n')
 
+#Criação de um dicionário para mapear cada linha com seu ID
+id_para_linha = {}
+for linha in linhas:
+    _linha = linha.split(' +++$+++ ')
+    if len(_linha) == 5:
+        id_para_linha[_linha[0]] = _linha[4]
