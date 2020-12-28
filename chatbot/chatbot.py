6104469de877388e386f829bb7ee5314c6ff84c6
mt-ssilva <matheus.ssilva@upe.br>
2020-12-28 12:22:25 -0300

    configuracao dos hiperparametros

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -292,6 +292,20 @@ def modelo_seq2seq(entradas, saidas, keep_prob, batch_size, tamanho_sequencia,
                                                                batch_size)
     
     return previsoes_treinamento, previsoes_teste
+    
+#--- Treinamento do modelo Seq2Seq ---
+#Configuração dos hiperparâmetros
+epocas = 100
+batch_size = 64
+rnn_tamanho = 512
+numero_camadas = 3
+tamanho_codificador_embeddings = 512
+tamanho_decodificador_embeddings = 512
+learnig_rate = 0.01
+learnig_rate_decaimento = 0.9
+min_learning_rate = 0.0001
+probabilidade_dropout = 0.5
+
 
 
 
